53cbf12b41b4adadfe0737eeec97e09424036b1d
Cameron Phillips <cameron0505@gmail.com>
2019-07-21 17:05:24 +0100

    Update doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,16 @@ use std::fs::File;
 use std::io;
 use std::io::Write;
 
+/// The maximum value for a single RGB component
 pub const RGB_MAX: u8 = 255;
 
-/// Convert an RGB f32 in [0.0, 1.0] to an u8 in [0, 255]
+/// Convert RGB `f32` in [0.0, 1.0] to `u8` in [0, 255]
 pub fn f32_to_u8(f: f32) -> u8 {
     assert!(f <= 1.0);
     (f * RGB_MAX as f32) as u8
 }
 
-/// Convert an RGB u8 in [0, 255] to an f32 in [0.0, 1.0]
+/// Convert RGB `u8` in [0, 255] to `f32` in [0.0, 1.0]
 pub fn u8_to_f32(u: u8) -> f32 {
     u as f32 / RGB_MAX as f32
 }
