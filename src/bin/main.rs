34576adcde7f53676be8c8ba34dd4dc2eff6db87
Cameron Phillips <cameron0505@gmail.com>
2019-07-21 15:54:40 +0100

    Updated example binary

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,22 @@
 use ppm::{Ppm, Rgb};
 
-fn main() {
-    let width = 64;
-    let height = width;
+fn string_to_ppm(s: &str) -> Ppm {
+    let bytes = s.as_bytes();
 
-    let mut ppm = Ppm::new(width, height);
+    let size = (bytes.len() as f32).sqrt().ceil() as usize;
+    let mut ppm = Ppm::new(size, size);
 
-    for x in 0..width {
-        for y in 0..height {
+    for x in 0..size {
+        for y in 0..size {
             if let Some(pixel) = ppm.get_mut(x, y) {
-                *pixel = Rgb::new(
-                    x as f32 / width as f32,
-                    y as f32 / height as f32,
-                    0.5);
+                
             }
         }
     }
 
-    ppm.save("image.ppm").unwrap();
+    ppm
+}
+
+fn main() {
+    string_to_ppm("cameron").save("image.ppm");
 }
