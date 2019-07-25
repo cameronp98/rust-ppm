fa221373d002240b09cbea8afa07df7e1e7966bb
Cameron Phillips <cameron0505@gmail.com>
2019-07-25 19:20:55 +0100

    extract util functions into their own module

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,9 @@ use std::fs::File;
 use std::io;
 use std::io::Write;
 
-/// The maximum value for a single RGB component
-pub const RGB_MAX: u8 = 255;
+mod util;
 
-/// Convert RGB `f32` in [0.0, 1.0] to `u8` in [0, 255]
-/// 
-/// # Examples
-/// 
-/// ```
-/// use ppm::{Ppm, Rgb, f32_to_u8};
-/// 
-/// assert_eq!(f32_to_u8(1.0), 255);
-/// ```
-pub fn f32_to_u8(f: f32) -> u8 {
-    assert!(f <= 1.0);
-    (f * RGB_MAX as f32) as u8
-}
-
-/// Convert RGB `u8` in [0, 255] to `f32` in [0.0, 1.0]
-/// 
-/// # Examples
-/// 
-/// ```
-/// use ppm::u8_to_f32;
-/// assert_eq!(u8_to_f32(255), 1.0);
-/// ```
-pub fn u8_to_f32(u: u8) -> f32 {
-    u as f32 / RGB_MAX as f32
-}
+pub use util::{RGB_MAX, f32_to_u8, u8_to_f32};
 
 /// An RGB pixel with values in [0.0, 1.0]
 #[derive(Debug, Clone)]
