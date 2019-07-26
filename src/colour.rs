daae0fefc8b5091b79d6666d803641e0d5a2dd92
Cameron Phillips <cameron0505@gmail.com>
2019-07-26 19:05:30 +0100

    Simplify colours

--- src/colour.rs
+++ src/colour.rs
@@ -1,40 +1,12 @@
-use crate::util::float_to_byte;
-
-/// An RGB pixel with values in [0.0, 1.0]
-#[derive(Debug, Clone)]
+#[derive(Clone, Default)]
 pub struct Rgb {
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
 impl Rgb {
-    /// Create a new Rgb colour
-    pub fn new(r: f32, g: f32, b: f32) -> Rgb {
+    pub fn new(r: u8, g: u8, b: u8) -> Rgb {
         Rgb { r, g, b }
-    }
-
-    pub fn as_bytes(&self) -> (u8, u8, u8) {
-        (
-            float_to_byte(self.r),
-            float_to_byte(self.g),
-            float_to_byte(self.b),
-        )
-    }
-
-    pub fn white() -> Rgb {
-        Rgb {
-            r: 1.0,
-            g: 1.0,
-            b: 1.0,
-        }
-    }
-
-    pub fn black() -> Rgb {
-        Rgb {
-            r: 0.0,
-            g: 0.0,
-            b: 0.0,
-        }
     }
 }
